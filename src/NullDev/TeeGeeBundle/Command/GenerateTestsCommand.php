6002994e42f2b7b7c2ebfbdf53435129da8cb029
Miro Svrtan <miro@mirosvrtan.me>
2015-02-21 22:00:07 +0100

    Added command for generating tests

---
+++ src/NullDev/TeeGeeBundle/Command/GenerateTestsCommand.php
@@ -0,0 +1,46 @@
+<?php
+namespace NullDev\TeeGeeBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+/**
+ * Class GenerateTestsCommand.
+ */
+class GenerateTestsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('gen:unit-tests')
+            ->setDescription('Generate unit tests')
+            ->addArgument(
+                'sourcePath',
+                InputArgument::REQUIRED,
+                'Where is the source Luke?'
+            );
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @SuppressWarnings("PHPMD.UnusedFormalParameter")
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        echo 'Generating unit tests'.PHP_EOL;
+
+        $sourcePath = realpath($input->getArgument('sourcePath'));
+
+        $service = $this->getContainer()->get('teegee.manager.class2test');
+
+        $service->run($sourcePath);
+
+        echo PHP_EOL.PHP_EOL.'Done'.PHP_EOL.PHP_EOL;
+    }
+}
